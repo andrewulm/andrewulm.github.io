d83170625ef07db13e73aea15e1c04085af8b9a9
Andrew Ulm <andrewulm@gmail.com>
2018-09-11 22:48:36 -0500

    05 work

--- 05-trivia/assets/js/game.js
+++ 05-trivia/assets/js/game.js
@@ -1,16 +1,48 @@
 // Trivia Game
 // Author: Andrew Ulm
 
+var question = [];
+var count = 0;
+
 $(document).ready( function () {
 
-    function getQuestions() {
+    function getQuestion() {
+
         $.getJSON('assets/js/questions.json', function (data) {
 
-            console.log(data);
+            var $question = $('<h3>').html(data.list[count].question);
 
-        })
+            var $choices = $('<ul>').addClass('choices');
+
+            $.each(data.list[count].choices, function (index) {
+                $($choices).append(
+                    '<li class="option">' + data.list[count].choices[index] + '</li>'
+                )
+            });
+
+            var $bundle = $('<div>').html($question)
+                .append($choices);
+
+            $('#_questions').append($bundle);
+
+        });
     }
 
-    getQuestions()
+    function startGame () {
+        getQuestion();
+    }
+
+    function evaluateChoice () {
+        console.log('ive been clicked!');
+    }
+
+    startGame();
+
+    $('.option').on('click', evaluateChoice);
 
 });
+
+// document.body.style.background = "blue";
+// setTimeout(function () {
+//    document.body.style.background = "yellow";
+// }, 2000);
